65bda4cb8262c6c2894e30c0be23d26a4fe1584f
anon2502 <jaan_nabeel@live.com>
2015-05-04 14:45:33 +1000

    implemented frame, added uml
    New UML file, use the program Violet to view and edit it
    game now creates a frame

--- src/ConnectFourGame.java
+++ src/ConnectFourGame.java
@@ -1,8 +1,44 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
 
-public class ConnectFourGame {
 
+public class ConnectFourGame extends JFrame implements KeyListener{
+	
+	public ConnectFourGame() {
+		
+		// set up frame
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addKeyListener(this);
+		setVisible(true);
+		setSize(400, 400);	// may need to make this dynamic
+		setTitle("Connect Four");
+
+		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("connect four game 2015");
+		
+		ConnectFourGame cfg = new ConnectFourGame();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
