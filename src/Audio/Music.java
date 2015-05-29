98c0920e7ebd7eccb15b1af5a0a75004b78b0989
Jaan <jaan_nabeel@live.com>
2015-05-29 17:32:23 +1000

    compile and run scripts

--- src/Audio/Music.java
+++ src/Audio/Music.java
@@ -3,6 +3,7 @@ package Audio;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -11,69 +12,122 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JOptionPane;
 
 
+/**
+ * @author Jaan
+ * 
+ * A class that allows for music to be played
+ * 
+ */
 public class Music {
 	
+	private static String audiorespath = "sounds/";
+	
 	// music clip names
-	static private String track0name = "bin/Audio/resource/nicedaytodie.wav";
+	private String track0name = audiorespath + "nicedaytodie.wav";
 	
 	// tracks
 	private ArrayList<String> tracks;
 	private int currenttrack;
 	
+	// sound effects
+	// names
+	static public String drop = audiorespath + "clang.wav";
+	static public String win = audiorespath + "imacstartup.wav";
+	static public String lose=  audiorespath + "baritone.wav";
+	static public String start =  audiorespath + "helloson.wav";
+	static public String colfull =  audiorespath + "sneeze.wav";
+	static public String quit =  audiorespath + "bye.wav";
+	
+	
 	// player
 	private AudioInputStream audioIn;
+	private AudioInputStream effectIn;
 	private Clip currentclip;
+	private Clip currentsound;
 	
 	// never play
-	boolean neverplay;
+	boolean disablesound;
 	
+	/**
+	 * constructor
+	 */
 	public Music(){
 	
-		// add tracks
+		// add music tracks
 		tracks = new ArrayList<String>();
 		tracks.add(track0name);
 		currenttrack = 0;
+		System.out.println(tracks.size());
 		
-		neverplay = false;
-			
+		// add sound effects		
+		disablesound = false;	
 	}
 	
 	/**
-	 * plays the current track from the given offset
+	 * plays the current track
 	 */
 	public void playTrack(){
-		if (!neverplay){
+		if (disablesound) return;
+
+		try {
+			audioIn = AudioSystem.getAudioInputStream(new File(tracks.get(currenttrack)));
+			AudioFormat format = audioIn.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
 			
-			try {
-				audioIn = AudioSystem.getAudioInputStream(new File(tracks.get(currenttrack)));
-				AudioFormat format = audioIn.getFormat();
-				DataLine.Info info = new DataLine.Info(Clip.class, format);
-				System.out.println(format.toString());
-				
-				currentclip = (Clip)AudioSystem.getLine(info);
-				currentclip.open(audioIn);
-				currentclip.loop(10);
-			} catch (UnsupportedAudioFileException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (LineUnavailableException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+			currentclip = (Clip)AudioSystem.getLine(info);
+			currentclip.open(audioIn);
+			currentclip.loop(10);
+		} catch (UnsupportedAudioFileException | IOException e) {
+//			 TODO Auto-generated catch block
+//			e.printStackTrace();
+			disablesound = true;
+			JOptionPane.showMessageDialog(null, "Sorry, Senpai, the music won't work!");
+		} catch (LineUnavailableException e){
+			JOptionPane.showMessageDialog(null, "Sorry, Senpai, the music won't work!");
+
+			disablesound = true;
 		}
+		
 	}
+	/**
+	 * stops the current track
+	 */
 	public void stop(){
 		currentclip.stop();
 	}
 	
+	/**
+	 * goes to and starts the next music track
+	 */
 	public void nextTrack(){
 		currenttrack++;
 		if (currenttrack == tracks.size()){
 			currenttrack = 0;
 		}
 		playTrack();
+	}
+	
+	/**
+	 * plays a sound effect
+	 * @param s the file path to the sound to be played
+	 */
+	public void playEffect(String s){
+		if (disablesound) return;
+		
+		try {
+			effectIn = AudioSystem.getAudioInputStream(new File(s));
+			AudioFormat format = effectIn.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			
+			currentsound = (Clip) AudioSystem.getLine(info);
+			currentsound.open(effectIn);
+		} catch (LineUnavailableException |UnsupportedAudioFileException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			disablesound = true;
+		}
 	}
 }
